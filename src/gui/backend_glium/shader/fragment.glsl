7a4407a367d8da7323901afee9aba88cf4f2c035
OLEGSHA <kvadropups@gmail.com>
2026-01-11 21:09:38 +0100

    Textures can now contain fully transparent pixels
    Not sure this is the best approach though...

--- src/gui/backend_glium/shader/fragment.glsl
+++ src/gui/backend_glium/shader/fragment.glsl
@@ -9,5 +9,8 @@ out vec4 color;
 
 void main() {
     color = texture(tex, texture_coords_passthru);
+    if (color.a != 1.0) {
+        discard;
+    }
     color *= vec4(color_multiplier_computed, 1.0);
 }
